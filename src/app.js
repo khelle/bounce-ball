04117d671fd8e76a12be7737deea2b5539d282c3
khelle <jamroz.kamil@gmail.com>
2016-11-03 01:30:17 +0100

    Non-issue : Added default server file

--- src/app.js
+++ src/app.js
@@ -1,4 +1,46 @@
-import { Person } from './Person/Person';
+import { Loop } from './Loop/Loop';
+import { Canvas } from './Canvas/Canvas';
+import { Ball } from './Canvas/Model/Ball';
 
-var p = new Person('Kamil');
-p.greet();
+class App
+{
+    /**
+     * @param {Array} args
+     */
+    constructor(args)
+    {
+        var loop = new Loop();
+        var canvas = new Canvas(args, loop);
+        var context = canvas.getContext();
+
+        canvas.getCanvas().addEventListener(
+            'click',
+            (e) => {
+                canvas.addElement.call(canvas, new Ball({
+                    posX: e.pageX,
+                    posY: e.pageY,
+                    angle: Math.floor(-Math.random()*140-20),
+                    speed: Math.floor(2+Math.random()*8),
+                    gravity: 0.1,
+                    elasticity: 0.4,
+                    friction: 0.01
+                }));
+            },
+            false
+        );
+
+        loop.start();
+    }
+}
+
+/**
+ * Run application after everything is loaded.
+ */
+window.addEventListener('load', () => {
+
+    var canvas = document.getElementById('playground');
+    canvas.height = window.innerHeight;
+    canvas.width = window.innerWidth;
+    window.app = new App(canvas);
+
+}, false);
